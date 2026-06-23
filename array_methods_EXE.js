413f504ef494c723238495ae642d327ab90e648a
Eli <elimermz@gmail.com>
2026-06-23 12:24:02 +0300

    question 4 filter for even numbers

--- array_methods_EXE.js
+++ array_methods_EXE.js
@@ -1,5 +1,12 @@
 //2
-const arr = ["hello", "world"];
+// const arr = ["hello", "world"];
 
-const upper = arr.map((str) => str.toUpperCase());
-console.log(upper);
+// const upper = arr.map((str) => str.toUpperCase());
+// console.log(upper);
+
+//4
+// const nums = [1, 2, 3, 4, 5, 6];
+
+// const evenNums = nums.filter((num) => !(num % 2));
+
+// console.log(evenNums);
