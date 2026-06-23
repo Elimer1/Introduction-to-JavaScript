22d3eb2d58b9edf4f4ba0376edccdf71fe18e999
Eli <elimermz@gmail.com>
2026-06-23 12:36:07 +0300

    10 getting product of list with reduce

--- array_methods_EXE.js
+++ array_methods_EXE.js
@@ -10,11 +10,19 @@
 // const evenNums = nums.filter((num) => !(num % 2));
 
 // console.log(evenNums);
+
 //8
-const prices = [50, 120, 80, 200, 150, 90];
+// const prices = [50, 120, 80, 200, 150, 90];
 
-const filterMap = prices
-  .filter((price) => price > 100)
-  .map((price) => Math.round(price * 1.1));
+// const filterMap = prices
+//   .filter((price) => price > 100)
+//   .map((price) => Math.round(price * 1.1));
 
-console.log(filterMap);
+// console.log(filterMap);
+
+//10
+const nums = [1, 2, 3, 4];
+
+const listProduct = nums.reduce((acc, curr) => acc * curr);
+
+console.log(listProduct);
