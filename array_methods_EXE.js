014097ac53a4de51c511f2824b4a572201a7613a
Eli <elimermz@gmail.com>
2026-06-23 12:29:06 +0300

    question 8 chaining filter and map

--- array_methods_EXE.js
+++ array_methods_EXE.js
@@ -10,3 +10,11 @@
 // const evenNums = nums.filter((num) => !(num % 2));
 
 // console.log(evenNums);
+//8
+const prices = [50, 120, 80, 200, 150, 90];
+
+const filterMap = prices
+  .filter((price) => price > 100)
+  .map((price) => Math.round(price * 1.1));
+
+console.log(filterMap);
